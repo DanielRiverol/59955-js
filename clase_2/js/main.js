c94d069320218a16cb0f0583f22ddcbe61e53b82
Daniel Riverol <dannyriverol@hotmail.com>
2024-07-23 10:39:46 -0300

    Actualizan

---
+++ clase_2/js/main.js
@@ -0,0 +1,35 @@
+//Variables
+let nombre; //declaración => no números ni caracteres especiales salvo _ o $
+nombre = "Pablo"; //asignación
+let apellido = "López"; //inicialización
+
+const CLAVE = "oruga"; //las constantes siempre se inicializan
+
+// nombre=prompt("ingresa tu nombre")
+// //alert(nombre)
+// console.log(nombre);
+
+// let nombre2 = prompt("Ingresa tu nombre en la constante");
+// console.log(nombre2);
+// nombre2 = prompt("Ingresa tu nuevo nombre en la constante");
+// console.log(nombre2);
+//CONDICIONALES si (condicion) entonces {algo}
+if (false) {
+  //bloque de ejeucion
+  console.log("Hola mundo");
+}
+
+//operadores = asignacion  == comparacion
+
+if (CLAVE == "Oruga") {
+  //false
+  console.log("Pudiste ingresar");
+}
+
+let temp = 30;
+
+if (temp < 30) {
+  console.log("Hace Calor");
+} else {
+  console.log("Hace frio");
+}
