56421c6b2d9d14fe4e2899e3a80a91ccd34a89c8
Daniel Riverol <dannyriverol@hotmail.com>
2024-07-23 12:05:07 -0300

    Actualizacion

--- clase_2/js/main.js
+++ clase_2/js/main.js
@@ -21,15 +21,106 @@ if (false) {
 
 //operadores = asignacion  == comparacion
 
-if (CLAVE == "Oruga") {
-  //false
-  console.log("Pudiste ingresar");
-}
+// if (CLAVE == "Oruga") {
+//   //false
+//   console.log("Pudiste ingresar");
+// }
 
-let temp = 30;
+// let temp = 29;
 
-if (temp < 30) {
-  console.log("Hace Calor");
+// if (temp > 30) {
+//   console.log("Hace Calor");
+// } else {
+//   console.log("Hace frio");
+// }
+
+// let talle = prompt("Ingresa la medida de tu cintura de pantalon");
+
+// if (talle >= 100) {
+//   console.log("Talle Xl");
+// } else if (talle >= 80) {
+//   console.log("Talle L");
+// } else if (talle >= 60) {
+//   console.log("Talle M");
+// } else if (talle >= 40) {
+//   console.log("Talle S");
+// } else {
+//   console.log("Talle XS");
+// }
+
+// let num= prompt("Ingresa tu edad")
+
+// if(num === 18){
+//     console.log("podes entrar");
+// }
+
+// if (isNaN(num)) {
+//     console.log(num);
+// }
+
+// let valorBoleano=  5 === 5
+// console.log(valorBoleano);
+// if(valorBoleano){
+//     console.log("Hola tarola");
+// }
+
+let usuarioGuardado = "Bart";
+let passGuardado = "elbarto";
+
+// let ingresoUsuario = prompt("Ingresa tu usuario");
+// let ingresoPass = prompt("Ingresa tu password");
+
+// //if anidado
+// if (usuarioGuardado === ingresoUsuario) {
+//    let ingresoPass = prompt("Ingresa tu password");
+//   if (passGuardado === ingresoPass) {
+//     console.log("Ya podes ingresar");
+//   } else {
+//     console.log("contrasena incorrecta");
+//   }
+// } else {
+//   console.log("Usuario no encontrado");
+// }
+
+//modulo division %
+//Operadores logicos &&   comparacion1 && comparacion2  devuelve true solo si las dos condiciones son true
+
+//  if(10 == 10 && "Hola"=="hola"){//false
+//     console.log('mensaje');
+//  }
+// let validacionUser = usuarioGuardado == ingresoUsuario;
+// let validacionPass = passGuardado == ingresoPass;
+// let validacionIngreso = validacionUser && validacionPass;
+
+// if (validacionIngreso) {
+//   console.log("Podes ingresar");
+// } else {
+//   console.log("credenciales incorrectas");
+// }
+
+//op OR || =>  comp1 || comp2,  true || true ,  true || false, false || true   #####  false | false => false
+
+// if (
+//   ingresoUsuario === "Bart" ||
+//   ingresoUsuario == "bart" ||
+//   ingresoUsuario === "BART"
+// ) {
+//   console.log("Hola Bart");
+// }
+
+//validacion de votantes
+
+let edadObligatoria = 18;
+let edadOptativa = 16;
+let edadLimite = 69;
+let edadUsuario = parseInt(prompt("Ingresa tu edad"));
+
+if (edadUsuario >= edadObligatoria && edadUsuario <= edadLimite) {
+  console.log("Esta obliggadom a votar");
+} else if (edadUsuario >= edadOptativa || edadUsuario > edadLimite) {
+  console.log("Podes voytar si queres");
 } else {
-  console.log("Hace frio");
+  console.log("No podes votar");
 }
+//Pasar las comparaciones a variables booleanas
+console.log("Fin del programa");
