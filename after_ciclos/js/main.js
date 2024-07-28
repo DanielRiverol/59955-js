84e8e8f8e392760a2d610cf341b7881a5638a156
Daniel Riverol <dannyriverol@hotmail.com>
2024-07-28 13:51:05 -0300

    Plantilla after

---
+++ after_ciclos/js/main.js
@@ -0,0 +1,14 @@
+//El cajero automático
+//Debemos generar un script para la operación de un cajero automático
+/* El mismo tiene que: 
+1- Mostrar un mensaje de bienvenida
+2- Permitir hasta 3 intentos de ingreso con el PIN del cliente
+  2a- Si ingresa correctamente dar mensaje de Bienvenida
+  2b- Terminados los intentos informar al cliente que se comunique con el banco
+3- Una vez ingresado mostrar las siguientes opciones en pantalla: 
+  a- Saldo
+  b- Retiro
+  c- Depósito
+  d- Salir 
+4- Al finalizar mostrar mensaje
+*/
