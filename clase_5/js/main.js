e3a23487e78aef40d9fc376d25e2e8f67a3a4439
Daniel Riverol <dannyriverol@hotmail.com>
2024-07-31 23:59:34 -0300

    clase_5

---
+++ clase_5/js/main.js
@@ -0,0 +1,11 @@
+let n1 = prompt("ingrese el primer numero");
+let n2 = prompt("ingrese el segundo numero");
+function suma(a, b) {
+  return a + b;
+}
+
+function resta(a, b) {
+  return a - b;
+}
+
+let resultado = suma(n1, n2);
