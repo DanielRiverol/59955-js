df2de963c925a03545ea81137b6df0cb890a5c01
Daniel Riverol <dannyriverol@hotmail.com>
2024-08-12 17:49:02 -0300

    clase_8

---
+++ clase_8/js/main.js
@@ -0,0 +1,13 @@
+
+const servicios = [
+  { id: 1, nombre: "limpieza de cutis", precio: 1200, img: "limpieza.jpg" },
+  { id: 2, nombre: "masajes", precio: 1800, img: "masajes.jpg" },
+  { id: 3, nombre: "electrodos", precio: 2800, img: "electrodos.jpg" },
+  { id: 4, nombre: "manicura", precio: 1500, img: "manicuria.webp" },
+  { id: 5, nombre: "uñas", precio: 1300, img: "esculpidas.webp" },
+  { id: 6, nombre: "mascarillas", precio: 1900, img: "mascarilla.jpeg" },
+  { id: 7, nombre: "microblanding", precio: 5800, img: "microblanding.png" },
+  { id: 8, nombre: "mascarillas", precio: 2300, img: "mascarilla.jpeg" },
+];
+
+
