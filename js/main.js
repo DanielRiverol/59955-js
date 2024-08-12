ba8045c3e3a3c47bb2f449d228e75d2df992db14
Daniel Riverol <dannyriverol@hotmail.com>
2024-08-12 17:35:37 -0300

    Actualizacion

--- js/main.js
+++ js/main.js
@@ -1,81 +1,125 @@
 let numeroComision = 59955; // Valor inicial
 const datos = [
-        { id: 1, nombre: 'Conceptos generales', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_1` },
-        { id: 2, nombre: 'Control de flujos', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_2` },
-        { id: 3, nombre: 'Bucles e iteraciones', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_3` },
-        { id: 4, nombre: 'Funciones', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_4` },
-        { id: 5, nombre: 'Objetos', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_5` },
-        { id: 6, nombre: 'Arrays', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_6` },
-        { id: 7, nombre: 'Funciones de orden Superior', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_7` },
-        // { id: 8, nombre: 'Funciones de orden Superior II', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/main/clase_8` },
-        // { id: 9, nombre: 'DOM', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_9` },
-        // { id: 10, nombre: 'Eventos', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_10` },
-        // { id: 11, nombre: 'Storage y JSON', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_11` },
-        // { id: 12, nombre: 'Operadores avanzados', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_12` },
-        // { id: 13, nombre: 'Operadores avanzados II + workshop', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_13` },
-        // { id: 14, nombre: 'Librerías', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_14` },
-        // { id: 15, nombre: 'Asincronía y promesas', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_15` },
-        // { id: 16, nombre: 'Ajax y Fetch', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_16` },
-        // { id: 17, nombre: 'Frameworks y Node.js', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_17` },
-        ];
-const afterClass=[
-        { id: 1, nombre: 'After ciclos', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_ciclos` },
-        { id: 2, nombre: 'After Búsqueda', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_busqueda` },
-        // { id: 3, nombre: 'After DOM', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_dom` },
-        ]
+  {
+    id: 1,
+    nombre: "Conceptos generales",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_1`,
+  },
+  {
+    id: 2,
+    nombre: "Control de flujos",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_2`,
+  },
+  {
+    id: 3,
+    nombre: "Bucles e iteraciones",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_3`,
+  },
+  {
+    id: 4,
+    nombre: "Funciones",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_4`,
+  },
+  {
+    id: 5,
+    nombre: "Objetos",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_5`,
+  },
+  {
+    id: 6,
+    nombre: "Arrays",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_6`,
+  },
+  {
+    id: 7,
+    nombre: "Funciones de orden Superior",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_7`,
+  },
+  {
+    id: 8,
+    nombre: "Funciones de orden Superior II",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/main/clase_8`,
+  },
+  // { id: 9, nombre: 'DOM', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_9` },
+  // { id: 10, nombre: 'Eventos', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_10` },
+  // { id: 11, nombre: 'Storage y JSON', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_11` },
+  // { id: 12, nombre: 'Operadores avanzados', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_12` },
+  // { id: 13, nombre: 'Operadores avanzados II + workshop', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_13` },
+  // { id: 14, nombre: 'Librerías', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_14` },
+  // { id: 15, nombre: 'Asincronía y promesas', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_15` },
+  // { id: 16, nombre: 'Ajax y Fetch', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_16` },
+  // { id: 17, nombre: 'Frameworks y Node.js', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/clase_17` },
+];
+const afterClass = [
+  {
+    id: 1,
+    nombre: "After ciclos",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_ciclos`,
+  },
+  {
+    id: 2,
+    nombre: "After Búsqueda",
+    enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_busqueda`,
+  },
+  // { id: 3, nombre: 'After DOM', enlace: `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/after_dom` },
+];
 
 document.title = `Comisión ${numeroComision}`;
-document.querySelector('h1').innerHTML = `Comisión ${numeroComision}`
-        
+document.querySelector("h1").innerHTML = `Comisión ${numeroComision}`;
+
 function actualizarDatos() {
-    // Actualizamos los enlaces con el nuevo valor
-    datos.forEach(fila => {
-        fila.enlace = `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/${fila.enlace.split('/')[8]}`;
-    });
-    // Llamamos a la función para agregar las filas
-    agregarFilasATabla();
+  // Actualizamos los enlaces con el nuevo valor
+  datos.forEach((fila) => {
+    fila.enlace = `https://github.com/DanielRiverol/${numeroComision}-js/tree/clases/${
+      fila.enlace.split("/")[8]
+    }`;
+  });
+  // Llamamos a la función para agregar las filas
+  agregarFilasATabla();
 }
 
 function agregarFilasATabla() {
-    const tabla = document.getElementById('tabla-dinamica');
-    const cuerpoTabla = tabla.getElementsByTagName('tbody')[0];
-    const tablaAfter = document.getElementById('tabla-dinamica-after');
-    const cuerpoTablaAfter = tablaAfter.getElementsByTagName('tbody')[0];
+  const tabla = document.getElementById("tabla-dinamica");
+  const cuerpoTabla = tabla.getElementsByTagName("tbody")[0];
+  const tablaAfter = document.getElementById("tabla-dinamica-after");
+  const cuerpoTablaAfter = tablaAfter.getElementsByTagName("tbody")[0];
 
-    cuerpoTabla.innerHTML = '';
-    cuerpoTablaAfter.innerHTML = '';
-            
-    //cargar clases
-    if(datos.length > 0){
-        datos.forEach((fila,index) => {
-            const nuevaFila = document.createElement('tr');
-            nuevaFila.innerHTML = `
+  cuerpoTabla.innerHTML = "";
+  cuerpoTablaAfter.innerHTML = "";
+
+  //cargar clases
+  if (datos.length > 0) {
+    datos.forEach((fila, index) => {
+      const nuevaFila = document.createElement("tr");
+      nuevaFila.innerHTML = `
                 <td>${fila.id}</td>
                 <td>${fila.nombre}</td>
-                <td><a href="${fila.enlace}" target="_blank">Clase ${index + 1}</a></td>`;
-            cuerpoTabla.appendChild(nuevaFila);
-            });
-        }else{
-            const nuevaFila = document.createElement('tr');
-            nuevaFila.innerHTML = `<td colspan="3">No disponible</td>`;
-            cuerpoTabla.appendChild(nuevaFila);
-           }
+                <td><a href="${fila.enlace}" target="_blank">Clase ${
+        index + 1
+      }</a></td>`;
+      cuerpoTabla.appendChild(nuevaFila);
+    });
+  } else {
+    const nuevaFila = document.createElement("tr");
+    nuevaFila.innerHTML = `<td colspan="3">No disponible</td>`;
+    cuerpoTabla.appendChild(nuevaFila);
+  }
 
-    //cargar afters  
-    if(afterClass.length > 0){
-        afterClass.forEach((fila,index) => {
-            const nuevaFila = document.createElement('tr');
-            nuevaFila.innerHTML = `
+  //cargar afters
+  if (afterClass.length > 0) {
+    afterClass.forEach((fila, index) => {
+      const nuevaFila = document.createElement("tr");
+      nuevaFila.innerHTML = `
                 <td>${fila.id}</td>
                 <td>${fila.nombre}</td>
                 <td><a href="${fila.enlace}" target="_blank">${fila.nombre}</a></td>`;
-            cuerpoTablaAfter.appendChild(nuevaFila);
-            });
-        }else{
-            const nuevaFila = document.createElement('tr');
-            nuevaFila.innerHTML = `<td colspan="3">No disponible</td>`;
-            cuerpoTablaAfter.appendChild(nuevaFila);
-           }
-        }
+      cuerpoTablaAfter.appendChild(nuevaFila);
+    });
+  } else {
+    const nuevaFila = document.createElement("tr");
+    nuevaFila.innerHTML = `<td colspan="3">No disponible</td>`;
+    cuerpoTablaAfter.appendChild(nuevaFila);
+  }
+}
 // Llamamos a la función para agregar las filas cuando se carga la página
 window.onload = agregarFilasATabla;
