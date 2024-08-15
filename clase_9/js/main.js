9f9b9a6a5decf4909221fd3a0f7a13fc8baa0e2a
Daniel Riverol <dannyriverol@hotmail.com>
2024-08-15 12:06:09 -0300

    Actualizacion

--- clase_9/js/main.js
+++ clase_9/js/main.js
@@ -27,3 +27,66 @@ const personas = [
   "Isabella",
   "Miguel",
 ];
+// selector por id
+const titulo = document.getElementById("principal");
+const secundario = document.getElementById("secundario");
+const contenedor = document.getElementById("contenedor");
+// console.log(titulo);
+// console.log(secundario);
+// console.log(contenedor);
+
+//selector por class
+const lista = document.getElementsByClassName("lista");
+const paises = document.getElementsByClassName("pais");
+// console.log(lista);
+// console.log(paises);
+// for (const pais of paises) {
+//   console.log(pais.innerHTML);
+// }
+//selector por tagname
+const divs = document.getElementsByTagName("div");
+// console.log(divs);
+paises[1].innerHTML = "<em>URUGUAY</em>";
+// paises[1].className = "card box"
+//crear nodos
+const parrafo = document.createElement("p");
+
+parrafo.innerHTML = "<em>Soy un parrafo creado desde JS</em>";
+
+contenedor.appendChild(parrafo);
+
+// titulo.remove()
+
+//acceder al valor de los inputs
+const inputs = document.querySelectorAll('input[type="text"]');
+
+const nombre = inputs[0];
+nombre.value ="DANI"
+console.log(nombre.value);
+
+const listAlumnos = document.querySelector("#nombres");
+
+personas.push("Gonza")
+personas.push("Julieta")
+for (const persona of personas) {
+  const li= document.createElement('li')
+  li.innerText= persona
+
+  listAlumnos.appendChild(li)
+}
+const producto = {
+  nombre: "Papas",
+  precio: 230,
+};
+let concatenado =
+  "Producto: " + producto.nombre + ", precio: $" + producto.precio;
+
+  let template= `PRODUCTO: ${producto.nombre}, 
+  PRECIO: $ ${producto.precio}`;//interpolacion
+// contenedor.innerText = concatenado;
+contenedor.innerText = template;
+
+contenedor.innerHTML = `<div class=""box>
+  <h3>${producto.nombre}</h3>
+  <p>${producto.precio}</p>
+</div>`;
