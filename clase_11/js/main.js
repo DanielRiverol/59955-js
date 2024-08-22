c2ae450fde40ead21b6606f6cc7c9e91bd5f1531
Daniel Riverol <dannyriverol@hotmail.com>
2024-08-22 12:01:20 -0300

    clase_11

--- clase_11/js/main.js
+++ clase_11/js/main.js
@@ -57,3 +57,72 @@ function crearHtml(arr) {
     contenedor.innerHTML = contenedor.innerHTML + html;
   }
 }
+//  crearHtml(servicios)
+
+// localStorage.setItem('saludo', 'Hola Flanders')
+localStorage.setItem("isValid", true);
+localStorage.setItem("edad", 20);
+
+const numeros = [1, 23, 4, 5, 6, 7, 78];
+localStorage.setItem("numeros", numeros);
+
+//ver los valores almacenados
+const saludo = localStorage.getItem("saludo");
+console.log(typeof saludo);
+const isValid = localStorage.getItem("isValid") === "true"; //trampa LS Guarda String
+let igaulA = 5 == 10; //variable booleana
+console.log(isValid);
+const nums = localStorage.getItem("numeros").split(",");
+console.log(nums);
+
+// localStorage.setItem('darkMode', 'activo')
+const toggleButton = document.getElementById("darkModeToggle");
+
+if (localStorage.getItem("darkMode") === "activo") {
+  document.body.classList.add("dark-mode");
+}
+///trabajar con el boton
+toggleButton.addEventListener("click", () => {
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", "inactivo");
+  } else {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "activo");
+  }
+});
+
+//limpiar el LS
+
+localStorage.removeItem("darkmode");
+localStorage.removeItem("saludo");
+//localStorage.clear() //elimina todas claves
+//sessionStorage
+// sessionStorage.setItem("sesion", "activa");
+const userObject = { nombre: "Pedro", edad: 36, pass: "1234" };
+
+const userJson = JSON.stringify(userObject); //convierto a json
+localStorage.setItem("usuario", userJson);
+
+let user = localStorage.getItem("usuario");
+user = JSON.parse(user); //convierto a obj
+console.log(user.nombre);
+
+const carrito = [];
+carrito.push(servicios[5]);
+carrito.push(servicios[3]);
+
+// console.log(carrito);
+
+localStorage.setItem("carrito", JSON.stringify(carrito));
+const btnMostrar = document.querySelector(".btn.btn-success");
+const btnLimpiar = document.querySelector(".btn.btn-delete");
+
+btnMostrar.addEventListener("click", () => {
+  const arrServ = JSON.parse(localStorage.getItem("carrito"));
+  console.log(arrServ);
+  crearHtml(arrServ);
+});
+btnLimpiar.addEventListener("click", () => {
+  localStorage.removeItem("carrito");
+});
